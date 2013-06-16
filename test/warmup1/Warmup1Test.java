15c0953a9bd98085506a040cb244aa3d935a6119
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-06-16 16:53:43 +0300

    Initial commit.

---
+++ test/warmup1/Warmup1Test.java
@@ -0,0 +1,28 @@
+package warmup1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Date: 16.06.13
+ * @author Sergey Kandalintsev
+ */
+public class Warmup1Test
+{
+    @Test
+    public void testDiff21() throws Exception
+    {
+        assertEquals( 2, Warmup1.diff21( 19 ) );
+        assertEquals( 11, Warmup1.diff21( 10 ) );
+        assertEquals( 0, Warmup1.diff21( 21 ) );
+        assertEquals( 2, Warmup1.diff21( 22 ) );
+        assertEquals( 8, Warmup1.diff21( 25 ) );
+        assertEquals( 18, Warmup1.diff21( 30 ) );
+        assertEquals( 21, Warmup1.diff21( 0 ) );
+        assertEquals( 20, Warmup1.diff21( 1  ) );
+        assertEquals( 19, Warmup1.diff21( 2  ) );
+        assertEquals( 22, Warmup1.diff21( -1  ) );
+        assertEquals( 23, Warmup1.diff21( -2 ) );
+        assertEquals( 58, Warmup1.diff21( 50 ) );
+    }
+}
