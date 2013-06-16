e3784f42bcb6ba7deb5ba088e769da3b15e6f536
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-06-16 16:54:06 +0300

    Initial commit.

--- src/warmup1/Warmup1.java
+++ src/warmup1/Warmup1.java
@@ -1,9 +1,21 @@
 package warmup1;
 
 /**
- *
+ *   Solutions for Exercises from http://codingbat.com/
  */
 public class Warmup1
 {
+    /**
+     * Given an int n, return the absolute difference between n and 21, except return double the absolute difference
+     * if n is over 21.
+     * <br/>
+     * <b/> diff21(19) --> 2
+     * <br/> diff21(10) --> 11
+     * <br/> diff21(21) --> 0
+     */
+    public static int diff21( int n )
+    {
+        return n > 21 ? 2 * (n - 21) : 21 - n;
+    }
 
 }
