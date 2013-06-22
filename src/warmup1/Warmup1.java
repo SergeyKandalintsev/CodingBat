30cddf300eba5cd5309acdb9cb995f6d629d3e03
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-06-22 19:28:48 +0300

    Class Warmup1 aded

--- src/warmup1/Warmup1.java
+++ src/warmup1/Warmup1.java
@@ -1,20 +1,75 @@
 package warmup1;
 
 /**
- *   Solutions for Exercises from http://codingbat.com/
+ * Solutions for Exercises from http://codingbat.com/
  */
 public class Warmup1
 {
     /**
+     * The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
+     * We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+     * <ul>
+     * <li>sleepIn(false, false) → true</li>
+     * <li>sleepIn(true, false) → false</li>
+     * <li>sleepIn(false, true) → true</li>
+     * </ul>
+     * @param weekday  <code>true</code> if it is a weekday, <code>false</code> otherwise
+     * @param vacation <code>true</code> if we are on vacation, <code>false</code> otherwise
+     * @return <code>true</code> if we sleep in, <code>false</code> otherwise
+     */
+    public static boolean sleepIn( boolean weekday, boolean vacation )
+    {
+        return !weekday || vacation;
+    }
+
+    /**
+     * We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in
+     * trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+     * <ul>
+     * <li>monkeyTrouble(true, true) → true</li>
+     * <li>monkeyTrouble(false, false) → true</li>
+     * <li>monkeyTrouble(true, false) → false</li>
+     * </ul>
+     * @param aSmile <code>true</code> if the monkey a is smiling, <code>false</code> otherwise
+     * @param bSmile <code>true</code> if the monkey b is smiling, <code>false</code> otherwise
+     * @return <code>true</code> if we are in trouble, <code>false</code> otherwise
+     */
+    public static boolean monkeyTrouble( boolean aSmile, boolean bSmile )
+    {
+        return !( aSmile || bSmile ) || ( aSmile && bSmile );
+    }
+
+    /**
+     * Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+     * <ul>
+     * <li>sumDouble(1, 2) → 3</li>
+     * <li>sumDouble(3, 2) → 5</li>
+     * <li>sumDouble(2, 2) → 8</li>
+     * </ul>
+     * @param a number
+     * @param b number
+     * @return Sum of the numbers, or double sum if the numbers are the same
+     */
+    public static int sumDouble( int a, int b )
+    {
+        int sum = a + b;
+
+        return a == b ? 2 * sum : sum;
+    }
+
+    /**
      * Given an int n, return the absolute difference between n and 21, except return double the absolute difference
      * if n is over 21.
-     * <br/>
-     * <b/> diff21(19) --> 2
-     * <br/> diff21(10) --> 11
-     * <br/> diff21(21) --> 0
+     * <ul>
+     * <li>diff21(19) --> 23</li>
+     * <li>diff21(10) --> 11</li>
+     * <li>diff21(21) --> 0</li>
+     * </ul>
+     * @param n number
+     * @return absolute difference between n and 21, or double the absolute difference if n is over 21.
      */
     public static int diff21( int n )
     {
-        return n > 21 ? 2 * (n - 21) : 21 - n;
+        return n > 21 ? 2 * ( n - 21 ) : 21 - n;
     }
 }
--- test/warmup1/Warmup1Test.java
+++ test/warmup1/Warmup1Test.java
@@ -1,14 +1,46 @@
 package warmup1;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Date: 16.06.13
- * @author Sergey Kandalintsev
+ * @author Sergey Kandalintsev  test  test
  */
 public class Warmup1Test
 {
+    @Test
+    public void testSleepIn() throws Exception
+    {
+        assertTrue( Warmup1.sleepIn( false, false ) );
+        assertFalse( Warmup1.sleepIn( true, false ) );
+        assertTrue( Warmup1.sleepIn( false, true ) );
+        assertTrue( Warmup1.sleepIn( true, true ) );
+    }
+
+    @Test
+    public void testMonkeyTrouble() throws Exception
+    {
+        assertTrue( Warmup1.monkeyTrouble( true, true ) );
+        assertTrue( Warmup1.monkeyTrouble( false, false ) );
+        assertFalse( Warmup1.monkeyTrouble( true, false ) );
+        assertFalse( Warmup1.monkeyTrouble( false, true ) );
+    }
+
+    @Test
+    public void testSumDouble() throws Exception
+    {
+        assertEquals( 3, Warmup1.sumDouble( 1, 2 ) );
+        assertEquals( 5, Warmup1.sumDouble( 3, 2 ) );
+        assertEquals( 8, Warmup1.sumDouble( 2, 2 ) );
+        assertEquals( -1, Warmup1.sumDouble( -1, 0 ) );
+        assertEquals( 12, Warmup1.sumDouble( 3, 3 ) );
+        assertEquals( 0, Warmup1.sumDouble( 0, 0 ) );
+        assertEquals( 1, Warmup1.sumDouble( 0, 1 ) );
+        assertEquals( 7, Warmup1.sumDouble( 3, 4 ) );
+    }
+
     @Test
     public void testDiff21() throws Exception
     {
@@ -19,9 +51,9 @@ public class Warmup1Test
         assertEquals( 8, Warmup1.diff21( 25 ) );
         assertEquals( 18, Warmup1.diff21( 30 ) );
         assertEquals( 21, Warmup1.diff21( 0 ) );
-        assertEquals( 20, Warmup1.diff21( 1  ) );
-        assertEquals( 19, Warmup1.diff21( 2  ) );
-        assertEquals( 22, Warmup1.diff21( -1  ) );
+        assertEquals( 20, Warmup1.diff21( 1 ) );
+        assertEquals( 19, Warmup1.diff21( 2 ) );
+        assertEquals( 22, Warmup1.diff21( -1 ) );
         assertEquals( 23, Warmup1.diff21( -2 ) );
         assertEquals( 58, Warmup1.diff21( 50 ) );
     }
