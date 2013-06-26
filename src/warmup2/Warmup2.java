b44f534ccebe28fe14a4107b9d3a8c543faf3f5c
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-06-26 17:44:23 +0300

    ClassWarmup2 updated.

--- src/warmup2/Warmup2.java
+++ src/warmup2/Warmup2.java
@@ -63,7 +63,9 @@ public class Warmup2
      * <li>countXX("xxx") → 2</li>
      * <li>countXX("xxxx") → 3</li>
      * </ul>
+     *
      * @param str given string
+     *
      * @return number of "xx" in the given string
      */
     public static int countXX( String str )
@@ -77,4 +79,169 @@ public class Warmup2
 
         return count;
     }
+
+    /**
+     * Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+     * <ul>
+     * <li>doubleX("axxbb") → true</li>
+     * <li>doubleX("axaxax") → false</li>
+     * <li>doubleX("xxxxx") → true</li>
+     * </ul>
+     *
+     * @param str given string
+     *
+     * @return <code>true</code> if the first instance of "x" in the string is immediately followed by another "x",
+     *         <code>false</code> otherwise
+     */
+    public static boolean doubleX( String str )
+    {
+        int i = str.indexOf( 'x' );
+
+        if ( i == -1 || i == str.length() - 1 )
+        {
+            return false;
+        }
+
+        return str.substring( i, i + 2 ).equals( "xx" );
+    }
+
+    /**
+     * Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+     * <ul>
+     * <li>stringBits("Hello") → "Hlo"</li>
+     * <li>stringBits("Hi") → "H"</li>
+     * <li>stringBits("Heeololeo") → "Hello"</li>
+     * </ul>
+     *
+     * @param str string
+     *
+     * @return String value according to exercise conditions
+     */
+    public static String stringBits( String str )
+    {
+        String result = "";
+
+        for ( int i = 0; i < str.length(); i += 2 )
+        {
+            result = result + str.charAt( i );
+        }
+
+        return result;
+    }
+
+    /**
+     * Given a non-empty string like "Code" return a string like "CCoCodCode".
+     * <ul>
+     * <li>stringSplosion("Code") → "CCoCodCode"</li>
+     * <li>stringSplosion("abc") → "aababc"</li>
+     * <li>stringSplosion("ab") → "aab"</li>
+     * </ul>
+     *
+     * @param str non-empty string
+     *
+     * @return String value according to exercise conditions
+     */
+    public static String stringSplosion( String str )
+    {
+        String result = "";
+
+        for ( int i = 0; i < str.length(); i++ )
+        {
+            result += str.substring( 0, i + 1 );
+        }
+        return result;
+    }
+
+    /**
+     * Given a string, return the count of the number of times that a substring length 2 appears in the string and
+     * also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+     * <ul>
+     * <li>last2("hixxhi") → 1</li>
+     * <li>last2("xaxxaxaxx") → 1</li>
+     * <li>last2("axxxaaxx") → 2</li>
+     * </ul>
+     *
+     * @param str given string
+     *
+     * @return String value according to exercise conditions
+     */
+    public static int last2( String str )
+    {
+        if ( str.length() < 2 )
+        {
+            return 0;
+        }
+
+        String end = str.substring( str.length() - 2 );
+        int count = 0;
+
+        for ( int i = 0; i < str.length() - 2; i++ )
+        {
+            String sub = str.substring( i, i + 2 );
+
+            if ( sub.equals( end ) )
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * Given an array of ints, return the number of 9's in the array.
+     * <ul>
+     * <li>arrayCount9({1, 2, 9}) → 1</li>
+     * <li>arrayCount9({1, 9, 9}) → 2</li>
+     * <li>arrayCount9({1, 9, 9, 3, 9}) → 3</li>
+     * </ul>
+     *
+     * @param nums array of ints
+     *
+     * @return number of 9's in the array
+     */
+    public static int arrayCount9( int[] nums )
+    {
+        int count = 0;
+
+        for ( int num : nums )
+        {
+            if ( num == 9 )
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * Given an array of ints, return true if one of the first 4 elements in the array is a 9. The array length may be
+     * less than 4.
+     * <ul>
+     * <li>arrayFront9({1, 2, 9, 3, 4}) → true</li>
+     * <li>arrayFront9({1, 2, 3, 4, 9}) → false</li>
+     * <li>arrayFront9({1, 2, 3, 4, 5}) → false</li>
+     * </ul>
+     *
+     * @param nums array of ints
+     *
+     * @return <code>true</code> if one of the first 4 elements in the array is a 9, <code>false</code> otherwise
+     */
+    public static boolean arrayFront9( int[] nums )
+    {
+        int len = nums.length > 4 ? 4 : nums.length;
+
+        for (int i = 0; i < len; i++)
+        {
+            if (nums[i] == 9)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
 }
