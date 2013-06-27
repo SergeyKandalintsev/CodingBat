14ec5a460fe3ca121b0c10abdc02666707c9510d
Sergey Kandalintsev <kandalintsev.sergey@gmail.com>
2013-06-27 18:18:22 +0300

    Class Ap1 updated.

--- src/ap1/Ap1.java
+++ src/ap1/Ap1.java
@@ -2,7 +2,6 @@ package ap1;
 
 /**
  * Date: 21.06.13
- *
  * @author Sergey Kandalintsev
  */
 public class Ap1
@@ -15,9 +14,7 @@ public class Ap1
      * <li>scoresIncreasing({1, 3, 2}) --> false</li>
      * <li>scoresIncreasing({1, 1, 4}) --> true</li>
      * </ul>
-     *
      * @param scores array of scores
-     *
      * @return <code>true</code> if each score is equal or greater than the one before. Otherwise returns
      *         <code>false</code>
      */
@@ -32,5 +29,32 @@ public class Ap1
         }
 
         return true;
+    }
+
+    /**
+     * Given an array of scores, return true if there are scores of 100 next to each other in the array. The array
+     * length will be at least 2.
+     * <ul>
+     * <li>scores100({1, 100, 100}) → true</li>
+     * <li>scores100({1, 100, 99, 100}) → false</li>
+     * <li>scores100({100, 1, 100, 100}) → truetrue</li>
+     * </ul>
+     * @param scores array of scores, array length at least 2
+     * @return <code>true</code> if there are scores of 100 next to each other in the array. Otherwise returns
+     *         <code>false</code>
+     */
+    public static boolean scores100( int[] scores )
+    {
+        boolean result = false;
+
+        for ( int i = 0; i < scores.length - 1; i++ )
+        {
+            if ( scores[i] == 100 && scores[i + 1] == 100 )
+            {
+                return true;
+            }
+        }
+
+        return result;
     }
 }
