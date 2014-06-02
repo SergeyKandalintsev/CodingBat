5751e92ab1e1304fd0eb3643e0162796612b1db9
Sergkand <kandalintsev.sergey@gmail.com>
2014-06-02 14:04:49 +0300

    Class String1 updated.

--- src/string1/String1.java
+++ src/string1/String1.java
@@ -144,15 +144,169 @@ public class String1 {
         return str.substring(0, str.length() / 2);
     }
 
+    /**
+     * Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length
+     * will be at least 2.
+     * <ul>
+     * <li>withoutEnd("Hello") --> "ell"</li>
+     * <li>withoutEnd("java") --> "av"</li>
+     * <li>withoutEnd("coding") --> "odin"</li>
+     * </ul>
+     *
+     * @param str
+     *         given string (at least 2 char length)
+     *
+     * @return string without the first and last char
+     */
+    public static String withoutEnd(String str) {
+        return str.substring(1, str.length() - 1);
+    }
+
+    /**
+     * Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside
+     * and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+     * <ul>
+     * <li>comboString("Hello", "hi") → "hiHellohi"</li>
+     * <li>comboString("hi", "Hello") → "hiHellohi"</li>
+     * <li>comboString("aaa", "b") → "baaab"</li>
+     * </ul>
+     *
+     * @param a
+     *         string
+     * @param b
+     *         string
+     *
+     * @return string of the form short+long+short
+     */
+    public static String comboString(String a, String b) {
+        return (a.length() < b.length()) ? (a + b + a) : (b + a + b);
+    }
+
+    /**
+     * Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least
+     * length 1.
+     * * <ul>
+     * <li>nonStart("Hello", "There") → "ellohere"</li>
+     * <li>nonStart("java", "code") → "avaode"</li>
+     * <li>nonStart("shotl", "java") → "hotlava"</li>
+     * </ul>
+     *
+     * @param a
+     *         string (at least 1 char length)
+     * @param b
+     *         string (at least 1 char length)
+     *
+     * @return concatenation of strings <code>a</code> and <code>b</code>, except omit the first char of each
+     */
+    public static String nonStart(String a, String b) {
+        return a.substring(1) + b.substring(1);
+    }
+
+    /**
+     * Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string
+     * length will be at least 2.
+     * <ul>
+     * <li>left2("Hello") → "lloHe"</li>
+     * <li>left2("java") → "vaja"</li>
+     * <li>left2("Hi") → "Hi"</li>
+     * </ul>
+     *
+     * @param str
+     *         given string (at least 2 char length)
+     *
+     * @return "rotated left 2" version of given string
+     */
+    public static String left2(String str) {
+        return str.substring(2) + str.substring(0, 2);
+    }
+
+    /**
+     * Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start. The string
+     * length will be at least 2.
+     * <ul>
+     * <li> right2("Hello") → "loHel"</li>
+     * <li>right2("java") → "vaja"</li>
+     * <li>right2("Hi") → "Hi"</li>
+     * </ul>
+     *
+     * @param str
+     *         given string (at least 2 char length)
+     *
+     * @return "rotated right 2" version of given string
+     */
+    public static String right2(String str) {
+        return str.substring(str.length() - 2) + str.substring(0, str.length() - 2);
+    }
+
+    /**
+     * Given a string, return a string length 1 from its front, unless front is false, in which case return a string
+     * length 1 from its back. The string will be non-empty.
+     * <ul>
+     * <li>theEnd("Hello", true) → "H"</li>
+     * <li>theEnd("Hello", false) → "o"</li>
+     * <li>theEnd("oh", true) → "o"</li>
+     * </ul>
+     *
+     * @param str
+     *         given string
+     * @param front
+     *         boolean
+     *
+     * @return a string length 1 from the front of given string if front is <code>true</code>. Otherwise returns
+     * a string length 1 from the back of given string.
+     */
+    public static String theEnd(String str, boolean front) {
+        return front ? str.substring(0, 1) : str.substring(str.length() - 1, str.length());
+    }
+
+    /**
+     * Given a string, return a version without both the first and last char of the string. The string may be any
+     * length, including 0.
+     * <ul>
+     * <li>withouEnd2("Hello") → "ell"</li>
+     * <li>withouEnd2("abc") → "b"</li>
+     * <li>withouEnd2("ab") → ""</li>
+     * </ul>
+     *
+     * @param str
+     *         given string
+     *
+     * @return a string without both the first and last char of the string
+     */
+    public static String withouEnd2(String str) {
+        return (str.length() <= 2) ? "" : str.substring(1, str.length() - 1);
+    }
+
+    /**
+     * Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri".
+     * The string length will be at least 2.
+     * <ul>
+     * <li>middleTwo("string") → "ri"</li>
+     * <li>middleTwo("code") → "od"</li>
+     * <li>middleTwo("Practice") → "ct"</li>
+     * </ul>
+     *
+     * @param str
+     *         string of even length
+     *
+     * @return a string made of the middle two chars
+     */
+    public static String middleTwo(String str) {
+        int halfLen = str.length() >> 1;
+        return str.substring(halfLen - 1, halfLen + 1);
+    }
+
+
     // -----------------------------------------------------------------------------------------
+
 
     /**
      * Given a string, if the string begins with "red" or "blue" return that color string, otherwise return the empty
      * string.
      * <ul>
-     * <li>seeColor("redxx") → "red"</li>
-     * <li>seeColor("xxred") → ""</li>
-     * <li>seeColor("blueTimes") → "blue"</li>
+     * <li>seeColor("redxx") --> "red"</li>
+     * <li>seeColor("xxred") --> ""</li>
+     * <li>seeColor("blueTimes") --> "blue"</li>
      * </ul>
      *
      * @param str
@@ -177,9 +331,9 @@ public class String1 {
      * Given a string, return true if the first 2 chars in the string also appear at the end of the string, such as
      * with "edited".
      * <ul>
-     * <li>frontAgain("edited") → true</li>
-     * <li>frontAgain("edit") → false</li>
-     * <li>frontAgain("ed") → true</li>
+     * <li>frontAgain("edited") --> true</li>
+     * <li>frontAgain("edit") --> false</li>
+     * <li>frontAgain("ed") --> true</li>
      * </ul>
      *
      * @param str
@@ -200,7 +354,111 @@ public class String1 {
     }
 
     public static String minCat(String a, String b) {
-        return null;
+        int minLength = Math.min(a.length(), b.length());
+
+        return a.substring(a.length() - minLength) + b.substring(b.length() - minLength);
+    }
+
+    public String extraFront(String str) {
+        String front = str.length() >= 2 ? str.substring(0, 2) : str;
+
+        return front + front + front;
+    }
+
+    /**
+     * Given a string, if a length 2 substring appears at both its beginning and end, return a string without the
+     * substring at the beginning, so "HelloHe" yields "lloHe". The substring may overlap with itself, so "Hi"
+     * yields "". Otherwise, return the original string unchanged.
+     * <ul>
+     * <li>without2("HelloHe") → "lloHe"</li>
+     * <li>without2("HelloHi") → "HelloHi"</li>
+     * <li>without2("Hi") → ""</li>
+     * </ul>
+     *
+     * @param str
+     *         given string
+     *
+     * @return
+     */
+    public static String without2(String str) {
+        if (str.length() < 2) {
+            return str;
+        }
+
+        String frontSubstring = str.substring(0, 2);
+        String backSubstring = str.substring(str.length() - 2);
+
+        if (frontSubstring.equals(backSubstring)) {
+            return str.substring(2);
+        }
+
+        return str;
+    }
+
+    /**
+     * Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a' and keep
+     * the second char if it is 'b'. The string may be any length. Harder than it looks.
+     * <ul>
+     * <li>deFront("Hello") → "llo"</li>
+     * <li>deFront("java") → "va"</li>
+     * <li>deFront("away") → "aay"</li>
+     * </ul>
+     *
+     * @param str
+     *
+     * @return
+     */
+    public static String deFront(String str) {
+        int len = str.length();
+
+        if (len == 0) {
+            return str;
+        }
+
+        if (len == 1) {
+            return str.charAt(0) == 'a' ? str : "";
+        }
+
+        if (str.charAt(0) == 'a') {
+            if (str.charAt(1) == 'b') {
+                return str;
+            } else {
+                return "a" + str.substring(2);
+            }
+        }
+
+        if (str.charAt(1) == 'b') {
+            return str.substring(1);
+        }
+
+        return str.substring(2);
+    }
+
+    /**
+     * Given a string and a second "word" string, we'll say that the word matches the string if it appears at the front
+     * of the string, except its first char does not need to match exactly. On a match, return the front of the string,
+     * or otherwise return the empty string. So, so with the string "hippo" the word "hi" returns "hi" and "xip"
+     * returns "hip". The word will be at least length 1.
+     * <ul>
+     * <li>startWord("hippo", "hi") → "hi"</li>
+     * <li>startWord("hippo", "xip") → "hip"</li>
+     * <li>startWord("hippo", "i") → "h"</li>
+     * </ul>
+     *
+     * @param str
+     * @param word
+     *
+     * @return
+     */
+    public static String startWord(String str, String word) {
+        String match = word.substring(1);
+        String front = str.substring(1, match.length());
+
+        if (match.equals(front)) {
+            return str.substring(0, word.length());
+        }
+
+        return "";
     }
 }
 
